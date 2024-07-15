9208bc6054e548d5dbedc98a9003c6b616abd074
JMN327 <173676271+JMN327@users.noreply.github.com>
2024-07-15 22:22:49 +0100

    Make changes up to 3. in the instructions

--- javascript.js
+++ javascript.js
@@ -4,52 +4,64 @@ let humanScore = 0;
 let computerScore = 0;
 
 
+const btnRock = document.querySelector("#btn-rock");
+const btnPaper = document.querySelector("#btn-paper");
+const btnScissors = document.querySelector("#btn-scissors");
 
-playGame();
 
-function playGame(){
-    console.log("Welcome to the game, have fun!")
-    // loop game over 5 rounds:
-    for (let index = 1; index < 6; index++) {
-        console.log(`Round ${index}, let's play!`) //rounds start message
-        let humanChoice = getHumanChoice(); //get player choice from prompt
-        let computerChoice = getComputerChoice(); // randomly generate computer choice
-        console.log("You chose: " + humanChoice); //display player choice
-        console.log("The computer chose: " + computerChoice); //display computer choice
-        let thisRound = playRound(humanChoice,computerChoice); //get result of choices
-        console.log(thisRound); //display result of choices
+btnRock.addEventListener("click", function (){
+    playGame("rock")
+});
+btnPaper.addEventListener("click", function (){
+    playGame("paper")
+});
+btnScissors.addEventListener("click", function (){
+    playGame("scissors")
+});
 
-        //calculate difference in score and display scores with feedback for all but the final round
-        let scoreDifference = humanScore-computerScore;
-        let commentary;
-        let winningCommentary = ["You're doing great!", "Keep going champ!", "Such tactics!"];
-        let drawingCommentary = ["It couldn't be closer!", "All to play for!", "The tension mounts!"]
-        let losingCommentary = ["Hang on in there!", "Don't give up!", "Can you make a comeback?"]
+function playGame(humanChoice){
+    
+    let computerChoice = getComputerChoice();
+    let thisRound = playRound(humanChoice,computerChoice);
 
+    const scores = document.querySelector("#scores");
+    const roundChoices = document.createElement("div");
+    roundChoices.classList.add("round-choices");
+    roundChoices.setAttribute('style', 'white-space: pre;')
+    roundChoices.textContent = "";
+    roundChoices.textContent = "You chose: " + humanChoice + "\r\nThe computer chose: " + computerChoice + "\r\n" + thisRound;
+    scores.appendChild(roundChoices);
+
+
+
+/*     //calculate difference in score and display scores with feedback for all but the final round
+    let scoreDifference = humanScore-computerScore;
+    let commentary;
+    let winningCommentary = ["You're doing great!", "Keep going champ!", "Such tactics!"];
+    let drawingCommentary = ["It couldn't be closer!", "All to play for!", "The tension mounts!"]
+    let losingCommentary = ["Hang on in there!", "Don't give up!", "Can you make a comeback?"]
+
+    if (scoreDifference === 0) {
+        commentary = drawingCommentary[Math.floor(Math.random()*drawingCommentary.length)];
+    } else if(scoreDifference > 0){
+        commentary = winningCommentary[Math.floor(Math.random()*winningCommentary.length)];
+    } else {
+        commentary = losingCommentary[Math.floor(Math.random()*losingCommentary.length)];
+    } */
+
+/*     if (index < 5) {
+        console.log(`The current scores are: You: ${humanScore} points, Computer: ${computerScore} points. ${commentary}`)
+    } else {
+        // give feedback for final round and end of the game
         if (scoreDifference === 0) {
-            commentary = drawingCommentary[Math.floor(Math.random()*drawingCommentary.length)];
+            commentary = "A draw... play again?";
         } else if(scoreDifference > 0){
-            commentary = winningCommentary[Math.floor(Math.random()*winningCommentary.length)];
+            commentary = "You won! This could be the start of something big";
         } else {
-            commentary = losingCommentary[Math.floor(Math.random()*losingCommentary.length)];
+            commentary = "You lost! Don't worry, it's just random anyway right?";
         }
-
-        if (index < 5) {
-            console.log(`The current scores are: You: ${humanScore} points, Computer: ${computerScore} points. ${commentary}`)
-        } else {
-            // give feedback for final round and end of the game
-            if (scoreDifference === 0) {
-                commentary = "A draw... play again?";
-            } else if(scoreDifference > 0){
-                commentary = "You won! This could be the start of something big";
-            } else {
-                commentary = "You lost! Don't worry, it's just random anyway right?";
-            }
-            console.log(`Finish! The scores are: You: ${humanScore} points, Computer: ${computerScore} points. ${commentary}`)
-        }
-
-
-    }
+        console.log(`Finish! The scores are: You: ${humanScore} points, Computer: ${computerScore} points. ${commentary}`)
+    } */
 
 }
 
@@ -126,38 +138,5 @@ function getComputerChoice(){
             console.log("the random value is: " + randomValue)
             break;
     }
-    
     return choice
-}
-
-function getHumanChoice(){
-    let choice; //variable to store the human's choice
-    //prompt for user input for first letter of rock, paper or scissors to avoid errors and mistakes.  
-    //also catches longer strings, upper case and nulls due to cancelling. 
-    //loops until correct input entered to avoid issues further into the program:
-    while (choice !== "r" && choice !== "p" && choice !== "s") {
-        choice = prompt("Please type 'r' to choose rock, 'p' to choose paper or 's' to choose scissors");
-        if (choice !== "" && choice !== null) {
-            choice = choice[0];
-            choice = choice.toLowerCase();
-        }
-    } 
-    //convert one letter inputs to full words
-    switch(choice){
-        case "r":
-            choice = "rock"
-            break;
-        case "p":
-            choice = "paper"
-            break;
-        case "s":
-            choice = "scissors"
-            break;
-        default:
-            choice = "something went wrong"
-            console.log("the erroneous choice input is: " + choice)
-            break;
-    }
-
-    return choice;
 }
